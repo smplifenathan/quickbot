8e6d6ac34a210663d200cd7eb8937a000d4b8677
Nic Mulvaney <nic@nicmulvaney.com>
2016-03-14 14:29:45 +0000

    variables

--- bot.js
+++ bot.js
@@ -6,6 +6,17 @@ var memwatch = require('memwatch-next');
 var GoogleSpreadsheet = require("google-spreadsheet");
 var bot, oldMessage;
 
+// if (!process.env.bot_token) {
+//     console.log('Error: Specify bot token in environment');
+//     process.exit(1);
+// }
+// if (!process.env.google_url) {
+//     console.log('Error: Specify google url in environment');
+//     process.exit(1);
+// }
+
+// process.env.google_url  = process.env.google_url.match(/[-\w]{25,}/);
+
 memwatch.on('leak', function(info) {
 	console.log('MEMORY LEAK')
 	console.log(info);
@@ -13,7 +24,7 @@ memwatch.on('leak', function(info) {
 
 var controller;
 
-var my_sheet = new GoogleSpreadsheet('1bUvxoGOKLqkhyNb6eTpiHkllFy2D0gFRGMhVrLNopzc');
+var my_sheet = new GoogleSpreadsheet(process.env.google_url.match(/[-\w]{25,}/) || '1bUvxoGOKLqkhyNb6eTpiHkllFy2D0gFRGMhVrLNopzc');
 var loadedData;
 
 var loadData = function(reload){
@@ -28,7 +39,7 @@ var loadData = function(reload){
 	});
 
 	bot = controller.spawn({
-		token: "xoxb-24874985728-F8NsGF5m5mIPnucBilxGldll"
+		token: process.env.bot_token || "xoxb-24874985728-F8NsGF5m5mIPnucBilxGldll"
 		// token: "xoxb-26588596465-70v8FMC5IoZPgNF9rMP7RkHS"
 	}).startRTM(function(){
 		console.log('ready');
@@ -115,117 +126,3 @@ var loadData = function(reload){
 loadData();
 
 
-
-// var controller = Botkit.slackbot({
-//     debug: true,
-// });
-
-// var bot = controller.spawn({
-//     token: "xoxb-24900082337-IDrUGCXSNKcPewbGHfda536H"
-// }).startRTM();
-
-
-// controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
-
-//     // bot.api.reactions.add({
-//     //     timestamp: message.ts,
-//     //     channel: message.channel,
-//     //     name: 'robot_face',
-//     // },function(err, res) {
-//     //     if (err) {
-//     //         bot.botkit.log('Failed to add emoji reaction :(',err);
-//     //     }
-//     // });
-
-
-//     controller.storage.users.get(message.user,function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message,'Hello ' + user.name + '!!');
-//         } else {
-//             bot.reply(message,'Hello.');
-//         }
-//     });
-// });
-
-controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
-    var matches = message.text.match(/call me (.*)/i);
-    var name = matches[1];
-    controller.storage.users.get(message.user,function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user,function(err, id) {
-            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-// controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
-
-//     controller.storage.users.get(message.user,function(err, user) {
-//         if (user && user.name) {
-//             bot.reply(message,'Your name is ' + user.name);
-//         } else {
-//             bot.reply(message,'I don\'t know yet!');
-//         }
-//     });
-// });
-
-
-// controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot, message) {
-
-//     bot.startConversation(message,function(err, convo) {
-
-//         convo.ask('Are you sure you want me to shutdown?',[
-//             {
-//                 pattern: bot.utterances.yes,
-//                 callback: function(response, convo) {
-//                     convo.say('Bye!');
-//                     convo.next();
-//                     setTimeout(function() {
-//                         process.exit();
-//                     },3000);
-//                 }
-//             },
-//         {
-//             pattern: bot.utterances.no,
-//             default: true,
-//             callback: function(response, convo) {
-//                 convo.say('*Phew!*');
-//                 convo.next();
-//             }
-//         }
-//         ]);
-//     });
-// });
-
-
-// controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
-
-//     var hostname = os.hostname();
-//     var uptime = formatUptime(process.uptime());
-
-//     bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
-// });
-
-// function formatUptime(uptime) {
-//     var unit = 'second';
-//     if (uptime > 60) {
-//         uptime = uptime / 60;
-//         unit = 'minute';
-//     }
-//     if (uptime > 60) {
-//         uptime = uptime / 60;
-//         unit = 'hour';
-//     }
-//     if (uptime != 1) {
-//         unit = unit + 's';
-//     }
-
-//     uptime = uptime + ' ' + unit;
-//     return uptime;
-// }
